07d0a3a0efb669fdf7f971ad04246de50332e249
eloch216 <48919455+eloch216@users.noreply.github.com>
2023-04-03 18:49:48 -0500

    Add test for partial matching

---
+++ tests/testthat/test-partial-matching.R
@@ -0,0 +1,14 @@
+# Partial matching when accessing list elements can cause problems. For example,
+# if values of `leaf_litter` but not `leaf` are supplied when creating a
+# `harvest_point` object, then a nonsensical value of `LMA` will be calculated
+# because `partitioning_component_weights$leaf_litter` will be a partial match
+# for the (unavailable) `partitioning_component_weights$leaf`.
+
+test_that("`leaf_litter` is not a partial match for `leaf`", {
+    expect_true(
+        is.na(process(harvest_point(
+            partitioning_component_weights = list(leaf_litter = 1),
+            partitioning_leaf_area = 2
+        ))$LMA)
+    )
+})
